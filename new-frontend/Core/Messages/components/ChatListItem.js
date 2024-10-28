9e0d2eb6fbb184985522452dec2fb9630e199691
Amine <87505961+madebyamine@users.noreply.github.com>
2024-10-28 02:11:31 +0100

    push chat

---
+++ new-frontend/Core/Messages/components/ChatListItem.js
@@ -0,0 +1,80 @@
+import { normalizeDate } from "../../misc/dateUtils.js";
+import { limitText } from "../../misc/textUtils.js";
+
+class ChatListItem extends HTMLElement {
+    constructor() {
+        super()
+        this.chatListItemElement = document.createElement('div')
+        this.appendChild(this.chatListItemElement);
+
+        this.conversaton = null
+        this.maxLength = 15
+    }
+
+    create() {
+        const target = this.conversaton.target
+        const lastMessage = this.conversaton.overview
+        let message = null;
+        let timestamp = null;
+
+        if (!target.first_name) {
+            target.first_name = target.username
+        }
+        if (lastMessage) {
+            message = lastMessage.message
+            timestamp = lastMessage.timestamp
+        }
+        this.chatListItemElement.className = 'd-flex align-items-center'
+        this.chatListItemElement.innerHTML = `
+            <div class="flex-shrink-0">
+                <img class="user-img" src="../assets/images/profile.jpg"
+                    alt="user img">
+                <span class="active"></span>
+            </div>
+            <div class="flex-grow-1 ms-3">
+                <h3>${target.first_name} ${target.last_name}</h3>
+                <p class="single-line-text">
+                    ${limitText(message, this.maxLength)}
+                    <span class="right-aligned-item">${normalizeDate(timestamp)}</span>
+                </p>
+            </div>
+        `
+        const p = this.chatListItemElement.querySelector('p')
+        // if (lastMessage && lastMessage.seen_at) {
+        //     p.innerHTML += `<span class="right-aligned-item">${normalizeDate(timestamp)}</span>`
+        // } else {
+        //     p.innerHTML += `<span class="unread-indicator start-100 badge rounded-pill bg-danger">1</span>`
+        // }
+        if (lastMessage && (lastMessage.seen_at || lastMessage.sender == user.id)) {
+            p.classList.add('text-white-50')
+        }
+    }
+
+    mute() {
+        const p = this.chatListItemElement.querySelector('p')
+        p.classList.add('text-white-50')
+    }
+
+    update(message) {
+        const p = this.chatListItemElement.querySelector('p')
+        p.firstChild.textContent = limitText(message.content, this.maxLength)
+        p.lastElementChild.textContent = normalizeDate(message.timestamp)
+        if (message.seen_at) {
+            p.classList.add('text-white-50')
+        } else {
+            p.classList.remove('text-white-50')
+        }
+    }
+
+
+    set config(data) {
+        this.conversaton = data
+        if (this.conversaton) {
+            this.create()
+        }
+    }
+}
+
+customElements.define('chat-list-element', ChatListItem)
+
+export default ChatListItem
---
+++ new-frontend/Core/Messages/components/Message.js
@@ -0,0 +1,43 @@
+import { normalizeDate } from "../../misc/dateUtils.js"
+import { limitText } from "../../misc/textUtils.js"
+
+class Message extends HTMLElement {
+    constructor() {
+        super()
+        this.messageElement = document.createElement('li')
+        this.appendChild(this.messageElement)
+
+        this.message = null
+        this.maxLength = 200
+        this.expanded = false
+    }
+
+    create() {
+        this.messageElement.className = (user.id == this.message.sender ? 'sender' : 'reply')
+        this.messageElement.innerHTML = `
+            <p> ${limitText(this.message.content, this.maxLength)} </p>
+            <span class="time">${normalizeDate(this.message.timestamp)}</span>
+        `
+        this.messageElement.onclick = this.expand
+        this.messageElement.addEventListener('click', () => {
+            if (!this.expanded) {
+                this.messageElement.firstElementChild.textContent = this.message.content
+                this.expanded = true
+            } else {
+                this.messageElement.firstElementChild.textContent = limitText(this.message.content, this.maxLength)
+                this.expanded = false
+            }
+        })
+    }
+
+    set config(data) {
+        this.message = data
+        if (this.message) {
+            this.create()
+        }
+    }
+}
+
+customElements.define('message-item', Message)
+
+export default Message
